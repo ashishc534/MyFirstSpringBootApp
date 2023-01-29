88a780d22c2139caf1267cc4fa9c7621606e0fb3
Ashish <ashishc534@gmail.com>
2023-01-29 16:33:52 +0530

    First spring boot app

---
+++ src/main/java/com/example/myfirstspringbootapp/MyFirstSpringBootAppApplication.java
@@ -0,0 +1,21 @@
+package com.example.myfirstspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyFirstSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstSpringBootAppApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "Ashish") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
